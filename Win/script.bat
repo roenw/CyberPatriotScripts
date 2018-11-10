2648e1e13398c640f085a012f7d8728f17a5ce28
Roen Wainscoat <13713430+roenw@users.noreply.github.com>
2018-11-09 21:08:37 -1000

    Premiminary checks before windows update starts

--- Win/script.bat
+++ Win/script.bat
@@ -1,10 +1,28 @@
 @echo off
-echo Checking if script contains Administrative rights...
+echo [ INFO ] Performing premilinary checks...
 
-if %errorLevel% == 0 (
-  echo The script has elevated permissions. Now proceeding.
+echo [ INFO ] Checking if script is running as Administrator...
+if %ERRORLEVEL% EQU 0 (
+  echo [ OK ] Script is running with elevated permissions
 ) else (
-  echo ERROR: Not run with elevated permissions. Please run again as administrator.
-  pause
+  cls
+  echo [ FAIL ] Script is not running as administrator
+  timeout 30 >nul
   exit
 )
+
+echo [ INFO ] Checking if computer has working internet connection...
+ping roen.us -n 3 -w 1000
+if %ERRORLEVEL% EQU 1 (
+  cls
+  echo [ FAIL ] Not connected to the internet
+  timeout 30 >nul
+  exit
+) else (
+  echo [ OK ] Connected to the internet
+)
+
+cls
+echo [ OK ] All preliminary checks have passed.
+pause
+
