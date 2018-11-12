1c652896ddcb6e4e026004f70002047df4ea468a
Roen Wainscoat <13713430+roenw@users.noreply.github.com>
2018-11-11 15:48:11 -1000

    Add color to status message

--- Win/Main.bat
+++ Win/Main.bat
@@ -1,135 +1,135 @@
 @echo off
-echo [ INFO ] Performing premilinary checks...
+echo [ [96mINFO[0m ] Performing premilinary checks...
 
-echo [ INFO ] Checking if script is running as Administrator...
+echo [ [96mINFO[0m ] Checking if script is running as Administrator...
 net session >nul 2>&1
 if %ERRORLEVEL% EQU 0 (
-  echo [ OK ] Script is running with elevated permissions
+  echo [ [92mOK[0m ] Script is running with elevated permissions
 ) else (
   cls
-  echo [ FAIL ] Script is not running as administrator
+  echo [ [91mFAIL[0m ] Script is not running as administrator
   timeout 30 >nul
   exit
 )
 
-echo [ INFO ] Checking if computer has working internet connection...
+echo [ [96mINFO[0m ] Checking if computer has working internet connection...
 ping roen.us -n 2 -w 1000
 if %ERRORLEVEL% EQU 1 (
   cls
-  echo [ FAIL ] Not connected to the internet
+  echo [ [91mFAIL[0m ] Not connected to the internet
   timeout 30 >nul
   exit
 ) else (
-  echo [ OK ] Connected to the internet
+  echo [ [92mOK[0m ] Connected to the internet
 )
 
 cls
-echo [ OK ] All preliminary checks have passed.
-echo [ INFO ] The script will start in 5 seconds. Click [ X ] if you wish to abort.
+echo [ [92mOK[0m ] All preliminary checks have passed.
+echo [ [96mINFO[0m ] The script will start in 5 seconds. Click [ X ] if you wish to abort.
 timeout 5 >nul
 cls
 
 REM -----------------------------------------------------------------------------------------
 
-echo [ INFO ] Disabling Guest account...
+echo [ [96mINFO[0m ] Disabling Guest account...
 net user Guest /active:no
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] Disabling Guest Account failed.
+  echo [ [91mFAIL[0m ] Disabling Guest Account failed.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] Guest account disabled
-timeout 5 >nul
+echo [ [92mOK[0m ] Guest account disabled
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
-echo [ INFO ] Disabling Admin account...
+echo [ [96mINFO[0m ] Disabling Admin account...
 net user Administrator /active:no
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] Disabling Admin Account failed.
+  echo [ [91mFAIL[0m ] Disabling Admin Account failed.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] Admin account disabled
-timeout 5 >nul
+echo [ [92mOK[0m ] Admin account disabled
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
-echo [ INFO ] Setting MAXPWAGE to 14 days...
+echo [ [96mINFO[0m ] Setting MAXPWAGE to 14 days...
 net accounts /maxpwage:14
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] An error occured while setting MAXPWAGE.
+  echo [ [91mFAIL[0m ] An error occured while setting MAXPWAGE.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] Maximum password life set.
-timeout 5 >nul
+echo [ [92mOK[0m ] Maximum password life set.
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
-echo [ INFO ] Setting MINPWLENGTH to 10 characters...
+echo [ [96mINFO[0m ] Setting MINPWLENGTH to 10 characters...
 net accounts /minpwlen:10
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] An error occured while setting MINPWLENGTH.
+  echo [ [91mFAIL[0m ] An error occured while setting MINPWLENGTH.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] Minimum password length set.
-timeout 5 >nul
+echo [ [92mOK[0m ] Minimum password length set.
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
-echo [ INFO ] Setting lockout duration to 45 minutes...
+echo [ [96mINFO[0m ] Setting lockout duration to 45 minutes...
 net accounts /lockoutduration:45
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] An error occured while setting lockout duration.
+  echo [ [91mFAIL[0m ] An error occured while setting lockout duration.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] Lockout duration policy is enforced.
-timeout 5 >nul
+echo [ [92mOK[0m ] Lockout duration policy is enforced.
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
-echo [ INFO ] Setting lockout threshold to 3 attempts...
+echo [ [96mINFO[0m ] Setting lockout threshold to 3 attempts...
 net accounts /lockoutthreshold:3
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] An error occured while setting lockout threshold.
+  echo [ [91mFAIL[0m ] An error occured while setting lockout threshold.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] Lockout threshold enforced.
-timeout 5 >nul
+echo [ [92mOK[0m ] Lockout threshold enforced.
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
-echo [ INFO ] Setting lockout window to 15 minutes...
+echo [ [96mINFO[0m ] Setting lockout window to 15 minutes...
 net accounts /lockoutwindow:15
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] An error occured while setting lockout window.
+  echo [ [91mFAIL[0m ] An error occured while setting lockout window.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] Lockout window enforced.
-timeout 5 >nul
+echo [ [92mOK[0m ] Lockout window enforced.
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
-echo [ INFO ] Begin auditing successful and unsuccessful logon/logoff attempts...
+echo [ [96mINFO[0m ] Begin auditing successful and unsuccessful logon/logoff attempts...
 auditpol /set /category:"Account Logon" /Success:enable /failure:enable
 auditpol /set /category:"Logon/Logoff" /Success:enable /failure:enable
 auditpol /set /category:"Account Management" /Success:enable /failure:enable
@@ -140,126 +140,126 @@ Auditpol /set /category:"Privilege use" /Success:enable /failure:enable
 Auditpol /set /category:"System" /failure:enable
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] An error occured while enabling logging for logon and logoff attempts.
+  echo [ [91mFAIL[0m ] An error occured while enabling logging for logon and logoff attempts.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] Now logging all logon and logoff attempts.
-timeout 5 >nul
+echo [ [92mOK[0m ] Now logging all logon and logoff attempts.
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
 
-echo [ INFO ] Disabling shutdown without logon...
+echo [ [96mINFO[0m ] Disabling shutdown without logon...
 REGEDIT.EXE  /S  "%~dp0\bundle\Disable_Shutdown_without_Logon.reg"
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] Executing premade regedit file to disable shutdown without logon failed.
+  echo [ [91mFAIL[0m ] Executing premade regedit file to disable shutdown without logon failed.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] Shutdown without logon disabled.
-timeout 5 >nul
+echo [ [92mOK[0m ] Shutdown without logon disabled.
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
-echo [ INFO ] Changing all user passwords except self...
+echo [ [96mINFO[0m ] Changing all user passwords except self...
 setlocal
 for /f "delims=" %%u in ('cscript //NoLogo %~dp0\bundle\GetLocalUsers.vbs') do (
   net user "%%u" "kalaheo 5up3r53cur3pa55w0rD$~"
 )
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] Changing passwords failed.
+  echo [ [91mFAIL[0m ] Changing passwords failed.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] All user passwords except self have been changed.
-timeout 5 >nul
+echo [ [92mOK[0m ] All user passwords except self have been changed.
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
-echo [ INFO ] Attempting to block FTP (20, 21)...
+echo [ [96mINFO[0m ] Attempting to block FTP (20, 21)...
 netsh advfirewall firewall add rule name="BlockFTP20" protocol=TCP dir=in localport=20 action=block
 netsh advfirewall firewall add rule name="BlockFTP21" protocol=TCP dir=in localport=21 action=block
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] An error occured while blocking FTP.
+  echo [ [91mFAIL[0m ] An error occured while blocking FTP.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] FTP is blocked.
-timeout 5 >nul
+echo [ [92mOK[0m ] FTP is blocked.
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
-echo [ INFO ] Attempting to block TCP/Telnet (23)...
+echo [ [96mINFO[0m ] Attempting to block TCP/Telnet (23)...
 netsh advfirewall firewall add rule name="BlockTelNet23" protocol=TCP dir=in localport=23 action=block
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] An error occured while blocking TelNet.
+  echo [ [91mFAIL[0m ] An error occured while blocking TelNet.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] TelNet is blocked.
-timeout 5 >nul
+echo [ [92mOK[0m ] TelNet is blocked.
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
-echo [ INFO ] Attempting to deny RDP access...
+echo [ [96mINFO[0m ] Attempting to deny RDP access...
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] An error occured while denying RDP.
+  echo [ [91mFAIL[0m ] An error occured while denying RDP.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] RDP connections are now being denied.
-timeout 5 >nul
+echo [ [92mOK[0m ] RDP connections are now being denied.
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
 
-echo [ INFO ] Attempting to force-check for updates and perform updates...
-echo [ INFO ] If the computer updates, you will have to restart the script to execute remaining actions.
+echo [ [96mINFO[0m ] Attempting to force-check for updates and perform updates...
+echo [ [96mINFO[0m ] If the computer updates, you will have to restart the script to execute remaining actions.
 cscript //NoLogo %~dp0\bundle\UpdateAllSoftware.vbs
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] Error updating Windows automatically.
+  echo [ [91mFAIL[0m ] Error updating Windows automatically.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] Windows updated!
-timeout 5 >nul
+echo [ [92mOK[0m ] Windows updated!
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
-echo [ INFO ] Attempting to enable Windows Firewall...
+echo [ [96mINFO[0m ] Attempting to enable Windows Firewall...
 NetSh Advfirewall set allprofiles state on
 Netsh Advfirewall show allprofiles
 
 if ERRORLEVEL 1 (
-  echo [ FAIL ] Error enabling Windows Firewall.
+  echo [ [91mFAIL[0m ] Error enabling Windows Firewall.
   timeout 30 >nul
   exit
 )
 
-echo [ OK ] Windows Firewall enabled.
-timeout 5 >nul
+echo [ [92mOK[0m ] Windows Firewall enabled.
+timeout 1 >nul
 
 REM -----------------------------------------------------------------------------------------
 
 
 cls
-echo [ OK ] The script has finished executing with no errors. Hope it helped your score out a bit!
-echo [ INFO ] It's likely that this script installed updates that require a restart.
-echo [ INFO ] Press any key to restart, or [ X ] to exit without restarting.
+echo [ [92mOK[0m ] The script has finished executing with no errors. Hope it helped your score out a bit!
+echo [ [96mINFO[0m ] It's likely that this script installed updates that require a restart.
+echo [ [96mINFO[0m ] Press any key to restart, or [ X ] to exit without restarting.
 pause >nul
 shutdown /r /t 0
